ca986bef3b6e63ff38cd720280b4a4e7dbd56dc3
Gregory Lee <grlee77@gmail.com>
2022-01-25 10:37:33 -0500

    DOC: update the Butterworth demo to illustrate how to reduce artifacts by padding

--- doc/examples/filters/plot_butterworth.py
+++ doc/examples/filters/plot_butterworth.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
@@ -9,7 +11,7 @@ image = data.camera()
 cutoffs = [.005, .02, .08, .16]
 
 
-def get_filtered(image, cutoffs, true_butterworth=False, order=3.0):
+def get_filtered(image, cutoffs, true_butterworth=False, order=3.0, npad=0):
     """Lowpass and highpass butterworth filtering at all specified cutoffs.
 
     Parameters
@@ -33,6 +35,8 @@ def get_filtered(image, cutoffs, true_butterworth=False, order=3.0):
 
     lowpass_filtered = []
     highpass_filtered = []
+    center_slice = tuple(slice(npad, s + npad) for s in image.shape)
+    image = np.pad(image, npad, mode='edge')
     for cutoff in cutoffs:
         lowpass_filtered.append(
             filters.butterworth(
@@ -41,7 +45,7 @@ def get_filtered(image, cutoffs, true_butterworth=False, order=3.0):
                 order=order,
                 high_pass=False,
                 true_butterworth=true_butterworth
-            )
+            )[center_slice]
         )
         highpass_filtered.append(
             filters.butterworth(
@@ -50,7 +54,7 @@ def get_filtered(image, cutoffs, true_butterworth=False, order=3.0):
                 order=order,
                 high_pass=True,
                 true_butterworth=true_butterworth
-            )
+            )[center_slice]
         )
     return lowpass_filtered, highpass_filtered
 
@@ -97,6 +101,28 @@ lowpasses, highpasses = get_filtered(image, cutoffs, true_butterworth=True)
 # cutoff and order.
 fig, axes = plot_filtered(lowpasses, highpasses, cutoffs)
 fig.text(0.5, 0.95, 'Filtering with true Butterworth filters (order=3.0)',
+         fontdict=dict(fontsize=18), horizontalalignment='center')
+
+
+# It can be seen that for lower values of the cutoff, there are artifacts near
+# the edge of the images. This is due to the periodic nature of the DFT and can
+# be reduced by applying some amount of padding to the edges prior to
+# filtering so that there are not sharp eges in the periodic extension of the
+# image.
+
+# Specifically, we now call our helper function with npad > 0 to
+# 1.) Pad the image by an amount, npad, on all sides.
+# 2.) Filter the padded image.
+# 3.) Crop the edges from the output to restore the original image extent.
+# In get_filtered we use numpy.pad with mode='edge' for smooth extension at the
+# boundary.
+lowpasses, highpasses = get_filtered(image, cutoffs, true_butterworth=True,
+                                     npad=64)
+
+# Note that with padding, the undesired shading at the image edges is
+# substantially reduced.
+fig, axes = plot_filtered(lowpasses, highpasses, cutoffs)
+fig.text(0.5, 0.95, 'Butterworth filtering with padded edges',
          fontdict=dict(fontsize=18), horizontalalignment='center')
 
 plt.show()
